0b8d1a7a61d5667316ba5e36cbe07dc6a4c21b29
Che <cgoranov@gmail.com>
2022-01-09 18:25:15 -0600

    copied over games_controller methods into api games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::GamesController < ApplicationController
+    before_action :set_game, only: [:show, :update, :destroy]
+
+  
+  def index
+    @games = Game.all
+
+    render json: @games
+  end
+
+  
+  def show
+    render json: @game
+  end
+
+  
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      render json: @game, status: :created, location: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  
+  def update
+    if @game.update(game_params)
+      render json: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  
+  def destroy
+    @game.destroy
+  end
+
+  private
+    
+    def set_game
+      @game = Game.find(params[:id])
+    end
+
+    def game_params
+      params.require(:game).permit(:name, :platform, :release_month)
+    end
+
 end
