6b746ff1b5b4c71e51fb99506e341115ccecb02f
Che <cgoranov@gmail.com>
2022-01-09 19:06:09 -0600

    in the create controller method, updated lcoation attribute of render

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -26,7 +26,7 @@ class Api::V1::GamesController < ApplicationController
     @game = Game.new(game_params)
 
     if @game.save
-      render json: @game, status: :created, location: @game
+      render json: @game, status: :created, location: api_v1_game_path(@game)
     else
       render json: @game.errors, status: :unprocessable_entity
     end
