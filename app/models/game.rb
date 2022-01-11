4df6f8908cd7b2960d9db03c0a29191c83e062d0
Che <cgoranov@gmail.com>
2022-01-10 20:08:14 -0600

    added callbck to Game class to downcase name for uniqueness validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,19 @@ class Game < ApplicationRecord
     has_many :comments
     validates :name, presence: true
     validates :name, uniqueness: true
+    before_save :downcase_name
 
     def comment_attributes=(comments_attributes)
         comments_attributes.each do |g|
             comment = Comment.create(content: g["content"])
             self.comments << comment
         end
+    end
+
+    private
+
+    def downcase_name
+        self.name = self.name.downcase
     end
 
 end
